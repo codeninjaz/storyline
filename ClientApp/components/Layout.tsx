a2e8faa0264d3d3e865298e0f72e76c0f01c8a01
shoobah <shoobah@gmail.com>
2017-02-21 22:20:16 +0100

    Lite store jox

--- ClientApp/components/Layout.tsx
+++ ClientApp/components/Layout.tsx
@@ -8,19 +8,19 @@ export interface ILayoutProps {
 export class Layout extends React.Component<ILayoutProps, void> {
     public render() {
         return <div className='container'>
-                <div className="header clearfix">
-                        <nav>
-                        <ul className="nav nav-pills pull-right">
-                            <li role="presentation"><Link className='btn btn-success' to={'/story/create'}>Create story</Link></li>
-                            <li role="presentation"><a href="#">Login</a></li>
-                        </ul>
-                        </nav>
-                        <h3 className="text-muted">
-                            <Link to={'/'}>
-                                Storyline
+            <div className="header clearfix">
+                <nav>
+                    <ul className="nav nav-pills pull-right">
+                        <li role="presentation"><Link className='btn btn-success' to={'/story/create'}>Create story</Link></li>
+                        <li role="presentation"><a href="#">Login</a></li>
+                    </ul>
+                </nav>
+                <h3 className="text-muted">
+                    <Link to={'/'}>
+                        Story!
                             </Link>
-                        </h3>
-                    </div>
+                </h3>
+            </div>
 
             <div className='row'>
                 <div className='col-sm-12'>
--- ClientApp/components/Story/View.tsx
+++ ClientApp/components/Story/View.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
 import { Link } from 'react-router';
+import { inject, observer } from 'mobx-react';
 
+@inject('storylineStore')
+@observer
 export class View extends React.Component<any, void> {
     public render() {
+        console.log('storylineStore', this.props.storylineStore);
+
         return <div>
+            <h1>TEST={this.props.storylineStore.test}</h1>
+            <div><Link to={'/story/edit/123'}><span className='glyphicon glyphicon-cog' /></Link></div>
 
-            <div><Link to={'/story/edit/123'}><span className='glyphicon glyphicon-cog'/></Link></div>
-
-            <div className='view'>                
+            <div className='view'>
                 <div className='voting-container'>
-                    <div><span className='glyphicon glyphicon-arrow-up'/></div>
+                    <div><span className='glyphicon glyphicon-arrow-up' /></div>
                     <div><span>10.5k</span></div>
-                    <div><span className='glyphicon glyphicon-arrow-down'/></div>
+                    <div><span className='glyphicon glyphicon-arrow-down' /></div>
                 </div>
 
                 <div className='story'>
@@ -22,27 +27,11 @@ export class View extends React.Component<any, void> {
             </div>
 
 
-            <div className='view'>                
+            <div className='view'>
                 <div className='voting-container'>
-                    <div><span className='glyphicon glyphicon-arrow-up'/></div>
+                    <div><span className='glyphicon glyphicon-arrow-up' /></div>
                     <div><span>10.5k</span></div>
-                    <div><span className='glyphicon glyphicon-arrow-down'/></div>
-                </div>
-
-                <div className='story'>
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
-
-                    <Link className='profile' to={'/profile/view/fluffmannen'}>fluffmannen</Link>
-                </div>
-            </div>
-
-
-
-            <div className='view'>                
-                <div className='voting-container'>
-                    <div><span className='glyphicon glyphicon-arrow-up'/></div>
-                    <div><span>10.5k</span></div>
-                    <div><span className='glyphicon glyphicon-arrow-down'/></div>
+                    <div><span className='glyphicon glyphicon-arrow-down' /></div>
                 </div>
 
                 <div className='story'>
@@ -54,11 +43,11 @@ export class View extends React.Component<any, void> {
 
 
 
-            <div className='view'>                
+            <div className='view'>
                 <div className='voting-container'>
-                    <div><span className='glyphicon glyphicon-arrow-up'/></div>
+                    <div><span className='glyphicon glyphicon-arrow-up' /></div>
                     <div><span>10.5k</span></div>
-                    <div><span className='glyphicon glyphicon-arrow-down'/></div>
+                    <div><span className='glyphicon glyphicon-arrow-down' /></div>
                 </div>
 
                 <div className='story'>
@@ -66,7 +55,23 @@ export class View extends React.Component<any, void> {
 
                     <Link className='profile' to={'/profile/view/fluffmannen'}>fluffmannen</Link>
                 </div>
-            </div>                                    
+            </div>
+
+
+
+            <div className='view'>
+                <div className='voting-container'>
+                    <div><span className='glyphicon glyphicon-arrow-up' /></div>
+                    <div><span>10.5k</span></div>
+                    <div><span className='glyphicon glyphicon-arrow-down' /></div>
+                </div>
+
+                <div className='story'>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+
+                    <Link className='profile' to={'/profile/view/fluffmannen'}>fluffmannen</Link>
+                </div>
+            </div>
         </div>;
     }
 }
