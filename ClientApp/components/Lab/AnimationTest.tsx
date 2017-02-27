4323489790d657106cc20244afa6cf9ad1c51720
Anders Sjöberg <shoobah@gmail.com>
2017-02-27 21:16:36 +0100

    Testanimation 1

--- ClientApp/components/Lab/AnimationTest.tsx
+++ ClientApp/components/Lab/AnimationTest.tsx
@@ -1,35 +1,58 @@
 import * as React from 'react';
 import Transition from 'react-motion-ui-pack';
+import { spring } from 'react-motion';
 
 interface ITestAnimationState {
-    items: [string];
+    items: string[];
+    num: number;
 }
 
 export class AnimationTest extends React.Component<any, ITestAnimationState> {
+
+    private interval: any;
+
     constructor(props: any) {
         super(props);
         this.state = {
-            items: [''],
+            items: [],
+            num: 0,
         };
-    }
-    public render() {
-        return (
-            <ul>
-                {/*{this.state.items.map((item, index) => (<li key={`testitem-${index}`}>{}</li>))}*/}
-                <li>Hej</li>
-            </ul>
-        );
+        this.interval = {};
     }
 
-    private componentDidMount() {
-        for (let i = 0; i < 100; i++) {
-            setTimeout(() => {
-                const newArray = this.state.items;
-                newArray.push(`nummer:${i}`);
-                this.setState(
-                    { items: newArray },
-                );
-            }, 1000);
-        }
+    public componentDidMount() {
+        this.resetStuff();
+    }
+
+    public render() {
+        return (<div style={{ overflow: 'hidden' }}>
+            <Transition
+                component={'ul'}
+                enter={{ opacity: 1, translateX: spring(0, { stiffness: 500, damping: 20 }) }}
+                leave={{ opacity: 0, translateX: 250 }}
+            >
+                {this.state.items.map((item, index) => (<li key={`testitem-${index}`}>{item}</li>))}
+            </Transition>
+        </div>);
+    }
+
+    private resetStuff() {
+        clearInterval(this.interval);
+        this.setState(
+            { items: [], num: 0 },
+        );
+        this.iterate();
+    }
+
+    private iterate() {
+        this.interval = setInterval(() => {
+            this.setState(
+                {
+                    items: this.state.items.concat([`nummer:${this.state.num}`]),
+                    num: this.state.num += 1,
+                },
+            );
+            if (this.state.num > 20) { this.resetStuff(); }
+        }, 300);
     }
 }
