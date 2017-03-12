3d86cb73465ccdb8fbb4a99a51cd48819e61789d
Unknown <johan.erik.obrink@gmail.com>
2017-03-12 12:35:58 +0100

    login lite snyggare

--- ClientApp/components/Login/Login.tsx
+++ ClientApp/components/Login/Login.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { inject } from 'mobx-react';
+import { Link } from 'react-router';
 
 @inject('storylineStore')
 export class Login extends React.Component<any, any> {
@@ -16,6 +17,10 @@ export class Login extends React.Component<any, any> {
         this.props.storylineStore.loginVisible = false;
     }
 
+    private handleClickExit() {
+        this.props.storylineStore.loginVisible = false;
+    }
+
     handleUserNameChange(event) {
         this.setState({usernamevalue: event.target.value});
     }
@@ -29,15 +34,18 @@ export class Login extends React.Component<any, any> {
     }
 
     public render() {
-        return <div className='login-wrapper'>
-                <div className='login'>
-                    <p>Login WIP (olaf/olaf2)</p>
-                    <input type='text' onChange={this.handleUserNameChange}/>
-                    <input type='password' onChange={this.handlePasswordChange}/>
-                    <input type='button' value='ok' onClick={() => this.handleSubmit()}/>
-                </div>
-                <div className='backdrop' onClick={() => this.handleClickOutside()}>
-                </div>
-            </div>;
+        return  <div className='login-wrapper'>
+                    <div className='login'>
+                        <header><span>Login</span><span className='close' onClick={() => this.handleClickExit()}>x</span></header>
+                        <input type='text' placeholder='Username' onChange={this.handleUserNameChange}/>
+                        <input type='password' placeholder='Password' onChange={this.handlePasswordChange}/>
+                        <input type='button' value='Submit' onClick={() => this.handleSubmit()}/>
+                        <input type='button' value='Facebook'/>
+                        <Link to={'/account/register'}>Register</Link><br/>
+                        <Link to={'/account/forgotpassword'}>Forgot password</Link>
+                    </div>
+                    <div className='backdrop' onClick={() => this.handleClickOutside()}>
+                    </div>
+                </div>;
     }
 }
